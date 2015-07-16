fb3f8b2c6a336e11205da22c389192e2387ce4fe
Alex Wolf <grindir@yandex.ru>
2015-07-16 14:09:40 +0300

    s

--- app/src/main/java/com/it_lab/teleport/MainActivity.java
+++ app/src/main/java/com/it_lab/teleport/MainActivity.java
@@ -2,28 +2,32 @@ package com.it_lab.teleport;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.net.Uri;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity  {
+public class MainActivity extends Activity  {
 
+    final String LOG_TAG = "myLogs";
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         listView=(ListView) findViewById(R.id.listView);
         DataAdapter adapter=new DataAdapter(this,initData());
         listView.setAdapter(adapter);
@@ -52,7 +56,6 @@ public class MainActivity extends ActionBarActivity  {
         Intent intent=new Intent(this,Stream.class);
         startActivity(intent);
     }
-
 
 
 
