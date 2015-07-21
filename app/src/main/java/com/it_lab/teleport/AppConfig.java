e04a32011b2f5811bbfdcfc99f904850bb348217
nort3d <nort3d@yandex.ru>
2015-07-21 17:10:44 +0300

    Merge branch 'master' of https://github.com/grindir/Teleport
    Conflicts:
    	app/build.gradle

--- app/src/main/java/com/it_lab/teleport/AppConfig.java
+++ app/src/main/java/com/it_lab/teleport/AppConfig.java
@@ -1,8 +1,6 @@
 package com.it_lab.teleport;
 
-/**
- * Created by alex on 15.07.15.
- */
+
 public class AppConfig {
     public static final String STREAM_URL = "rtsp://85.140.46.133:8113/videochat/stream2";
     public static final String PUBLISHER_USERNAME = "ravi";
