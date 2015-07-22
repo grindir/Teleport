3532bd2e8689362ef1b5f48a4149c6bdf9295ccb
Alex Wolf <grindir@yandex.ru>
2015-07-22 11:56:17 +0300

    asd

--- app/src/main/java/com/it_lab/teleport/AppConfig.java
+++ app/src/main/java/com/it_lab/teleport/AppConfig.java
@@ -4,7 +4,7 @@ package com.it_lab.teleport;
  * Created by alex on 15.07.15.
  */
 public class AppConfig {
-    public static final String STREAM_URL = "rtsp://192.168.0.210:9999/stream";
+    public static final String STREAM_URL = "rtsp://192.168.0.210:7070/stream";
     public static final String PUBLISHER_USERNAME = "ravi";
     public static final String PUBLISHER_PASSWORD = "passtemp";
 }
