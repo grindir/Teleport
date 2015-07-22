cd01e4276f6b84d9993a4caa7e7222c74e6c9cb8
nort3d <nort3d@yandex.ru>
2015-07-22 11:57:03 +0300

    Merge branch 'master' of https://github.com/grindir/Teleport
    Conflicts:
    	app/build.gradle

--- app/src/main/java/com/it_lab/teleport/MainActivity.java
+++ app/src/main/java/com/it_lab/teleport/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,11 +24,11 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         listView=(ListView) findViewById(R.id.listView);
         RequestAdapter adapter=new RequestAdapter(this,initData());
         listView.setAdapter(adapter);
+
+        createTab();
 
     }
 
@@ -46,16 +47,7 @@ public class MainActivity extends ActionBarActivity {
         return list;
     }
 
-    public void openRequestMe(View view)
-    {
-        Intent intent=new Intent(this,RequestMeActivity.class);
-        startActivity(intent);
-    }
-    public void openMyRequest(View view)
-    {
-        Intent intent=new Intent(this,MyRequestActivity.class);
-        startActivity(intent);
-    }
+
 
     public void startStream(View view)
     {
@@ -63,6 +55,26 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
+    //ПРОВЕРЕН
+    private void createTab(){
+
+        TabHost tabs = (TabHost) findViewById(R.id.tabHost);
+
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
+
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Посмотреть");
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Показать");
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+    }
 
 
 }
